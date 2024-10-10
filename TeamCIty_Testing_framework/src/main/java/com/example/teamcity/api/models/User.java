06966cc58f35e5574b0430b68fb9869c12a15047
Moser <moserturbo@gmail.com>
2024-10-10 23:03:14 +0200

    Add config, base, test, spec for api-tests

---
+++ TeamCIty_Testing_framework/src/main/java/com/example/teamcity/api/models/User.java
@@ -0,0 +1,17 @@
+// файлик с сущностью пользователя
+
+package com.example.teamcity.api.models;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+import lombok.Builder;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String username;
+    private String password;
+}
